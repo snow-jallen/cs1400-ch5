a7e7a1d73fcedbc6081363a9fd23e6e9cee5e01d
Jonathan Allen <jallen@snow.edu>
2019-10-17 16:55:15 +0000

    Prepping to teach 5.11 for cs1400

--- main.cpp
+++ main.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World\n";
-    cout << "Input: ";
-    string data;
-    getline(cin, data);
-    cout << "Output: " << data << "\n\n";
+    cout << "Opening output file..." << endl;
+    ofstream outputFile;
+    outputFile.open("output.txt", ios::out);
+    outputFile << "File-Based Adding Machine" << endl;
+
+    cout << "Opening input file..." << endl;
+    ifstream inputFile;
+    inputFile.open("input.txt");
+    int num;
+    int total = 0;
+
+    cout << "Reading input file..." << endl;
+    while(inputFile >> num)
+    {
+        total += num;
+    }
+    inputFile.close();
+
+    cout << "Total written to output file is " << total << endl;
+    outputFile << "The total is" << endl;
+    outputFile << total;
+    outputFile.close();
+
+    cout << "All done!" << endl;
     return 0;
 }
